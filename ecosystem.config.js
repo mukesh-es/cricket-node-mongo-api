de5d105b54da60a31a4047a68a38bc5b9b488165
ranv1994 <122173739+ranv1994@users.noreply.github.com>
2025-12-24 17:05:49 +0530

    Configure cluster mode and memory settings

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,19 @@ module.exports = {
   apps: [
     {
       name: "cricket-node-mongo-api",
-      script: "./index.js"
-    },
-  ],
+      script: "./index.js",
+      exec_mode: "cluster",
+      instances: 3, // NOT max
+
+      max_memory_restart: "600M",
+      listen_timeout: 8000,
+      kill_timeout: 8000,
+
+      merge_logs: true,
+      time: true,
+
+      node_args: "--max-old-space-size=512"
+    }
+  ]
 };
+
