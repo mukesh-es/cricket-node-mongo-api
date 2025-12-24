073f39fc46be1cde06aca6b1cd3d85e4f26da9e6
ranv1994 <122173739+ranv1994@users.noreply.github.com>
2025-12-24 16:39:24 +0530

    Refactor MongoDB connection with improved settings
    Updated MongoDB connection settings and added logging.

--- db/mongoDB.js
+++ db/mongoDB.js
@@ -2,17 +2,20 @@ const mongoose = require('mongoose');
 const { errorWithTime } = require('../helpers/loggerHelper');
 
 const connectMongoDB = async () => {
-    try{
-        const DBURI = `${process.env.MONGODB_URI}`
-        await mongoose.connect(DBURI, {
-            maxPoolSize: 10,
-            minPoolSize: 2,
-            serverSelectionTimeoutMS: 5000,
-            socketTimeoutMS: 45000
-        });
-    }catch(err){
-        errorWithTime('MongoDB connection failed', err)
-    }
-}
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      maxPoolSize: 5,        // IMPORTANT
+      minPoolSize: 1,        // IMPORTANT
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 30000,
+      heartbeatFrequencyMS: 10000,
+    });
+
+    console.log('MongoDB connected');
+  } catch (err) {
+    errorWithTime('MongoDB connection failed', err);
+    process.exit(1); // fail fast
+  }
+};
 
 module.exports = connectMongoDB;
