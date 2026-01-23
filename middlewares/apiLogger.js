0382b1f57c7accc191825db573c93ba279e82e2b
mukesh-es <mukesh.kumar@entitysport.com>
2026-01-23 18:02:41 +0530

    File log enable disable from db

--- middlewares/apiLogger.js
+++ middlewares/apiLogger.js
@@ -1,49 +1,52 @@
 const { formatDateTime } = require('../utils/dateUtils');
 const path = require('path');
 const fs = require('fs');
+const { getConfigSync } = require('../helpers/configHelper');
 
 const apiLogger = (req, res, next) => {
-  const logDir = path.join(__dirname, '..', 'logs', 'api');
+  const config = getConfigSync();
   const currentTime = formatDateTime();
   const requestURL = req.originalUrl;
 
-  if (!fs.existsSync(logDir)) {
-    fs.mkdirSync(logDir, { recursive: true });
+  if(config.file_log  && config.file_log === true){
+      const logDir = path.join(__dirname, '..', 'logs', 'api');
+
+      if (!fs.existsSync(logDir)) {
+        fs.mkdirSync(logDir, { recursive: true });
+      }
+      const date = new Date().toISOString().split('T')[0];
+      const logFile = path.join(logDir, `${date}.log`);
+
+      const ip =
+        req.headers['x-forwarded-for']?.split(',')[0] ||
+        req.socket.remoteAddress ||
+        'unknown';
+      
+      let platform = req.headers['x-client-platform'] || 'unknown';
+
+      if (platform === 'unknown') {
+        const ua = req.headers['user-agent'] || '';
+        if (/android|iphone|ipad|ios/i.test(ua)) {
+          platform = 'app';
+        } else if (/mozilla|chrome|safari|firefox/i.test(ua)) {
+          platform = 'web';
+        }
+      }
+
+      const logData = [
+        currentTime,
+        requestURL,
+        ip,
+        platform,
+        req.query || {},
+      ];
+
+      fs.appendFile(
+        logFile,
+        JSON.stringify(logData) + '\n',
+        () => {} // non-blocking
+      );
   }
-  const date = new Date().toISOString().split('T')[0];
-  const logFile = path.join(logDir, `${date}.log`);
-
-  const ip =
-    req.headers['x-forwarded-for']?.split(',')[0] ||
-    req.socket.remoteAddress ||
-    'unknown';
-  
-  let platform = req.headers['x-client-platform'] || 'unknown';
-
-  if (platform === 'unknown') {
-    const ua = req.headers['user-agent'] || '';
-    if (/android|iphone|ipad|ios/i.test(ua)) {
-      platform = 'app';
-    } else if (/mozilla|chrome|safari|firefox/i.test(ua)) {
-      platform = 'web';
-    }
-  }
-
-  const logData = {
-    time: currentTime,
-    method: req.method,
-    url: requestURL,
-    ip,
-    platform,
-    query: req.query || {},
-    body: req.body || {}
-  };
-
-  fs.appendFile(
-    logFile,
-    JSON.stringify(logData) + '\n',
-    () => {} // non-blocking
-  );
 
 
   // Override res.json to print response
