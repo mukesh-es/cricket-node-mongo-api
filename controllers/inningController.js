704ea937ccd9fa88683afca9502aa47245b0b0d0
mukesh-es <mukesh.kumar@entitysport.com>
2026-04-29 16:25:39 +0530

    Inning oddsgraph filter response changed for inning commentaryoddsgraph

--- controllers/inningController.js
+++ controllers/inningController.js
@@ -7,13 +7,67 @@ const { getContextValue } = require('../middlewares/requestContext');
 exports.fieldData = async(req, res) => {
     try{
         let {matchId, inningNumber, resource} = req.params;
-        let {order, paged, per_page} = req.query;
+        let {order, paged, per_page, newparam} = req.query;
         const apiName = getContextValue('api_name');
         const fieldName = getFieldName(apiName);
+        const isCommentary = resource === 'commentary';
         if(!inningNumber || inningNumber <= 0){
             inningNumber = 1;
         }
         paged = Number(paged) || 1;
+
+        // newparam exists, ignore inningNumber
+        if (isCommentary && newparam) {
+            const filters = {
+                match_id: Number(matchId)
+            };
+
+            let allInnings = await InningModel.find(
+                    { match_id: Number(matchId) },
+                    { inning_id: 1, [fieldName]: 1, iid: 1 }
+                );
+            
+            let result = {};
+            if(allInnings){
+                allInnings.forEach(inning => {
+                    const parsed =
+                        typeof inning[fieldName] === 'string'
+                            ? JSON.parse(inning[fieldName])
+                            : inning[fieldName];
+                    
+                    const battingTeamId = parsed?.inning?.batting_team_id;
+
+                    let teamAbbr = null;
+
+                    if (parsed?.teams?.teama?.tid === battingTeamId) {
+                        teamAbbr = parsed.teams.teama.abbr;
+                    } else if (parsed?.teams?.teamb?.tid === battingTeamId) {
+                        teamAbbr = parsed.teams.teamb.abbr;
+                    }
+
+                    let commentariesArray = Array.isArray(parsed?.commentaries)
+                        ? [...parsed.commentaries]
+                        : [];
+
+                    const filtered = commentariesArray
+                                .filter(item => item?.event !== "overend") // 👈 skip
+                                .map(item => ({
+                                    over: item.over,
+                                    ball: item.ball,
+                                    inning_score: item.inning_score,
+                                    team_win_percentage: item.team_win_percentage,
+                                    teamback: item.teamback,
+                                    teamlay: item.teamlay,
+                                    teamoddstype: item.teamoddstype,
+                                    team_id: battingTeamId,
+                                    team_abbr: teamAbbr
+                                }));
+
+                    result[inning.iid] = filtered;
+                })
+            }
+            return requestSuccess({res, result});
+        }
 
         const filters = {
             match_id: Number(matchId),
